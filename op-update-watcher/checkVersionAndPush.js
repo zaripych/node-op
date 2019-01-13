39d8005d9586aa5f02ec5621bae38725b6c45dd1
Rinat Zaripov <zaripov.rinat@gmail.com>
2019-01-13 12:20:12 +1100

    chore: automatic updates

---
+++ op-update-watcher/checkVersionAndPush.js
@@ -0,0 +1,171 @@
+const { spawnSync } = require("child_process");
+const { writeFileSync, readFileSync, existsSync } = require("fs");
+const path = require("path");
+const os = require("os");
+const { gt, gte } = require("semver");
+const { determineLatestVersion } = require("./determineLatestVersion");
+const rimraf = require("rimraf");
+
+const pack = require("../package.json");
+const packageOpVersion = pack["op_version"];
+
+const rootDir = path.join(__dirname, "../");
+const cloneDirName = "node-op-temp";
+const cloneDir = path.join(rootDir, "node-op-temp");
+
+const gitUrl = repoCreds =>
+  `https://${repoCreds ? repoCreds + "@" : ""}github.com/zaripych/node-op.git`;
+
+const url = gitUrl(process.env.GH_TOKEN);
+const branch = "master";
+const pushBranch = "fix/upgrade-op-version";
+
+function updatePackageJsonIfRequired(version) {
+  const packageJsonPath = path.join(cloneDir, "package.json");
+
+  const contents = readFileSync(packageJsonPath, { encoding: "utf8" });
+
+  const packageJson = JSON.parse(contents);
+
+  const oldVersion = packageJson["op_version"];
+
+  if (gte(oldVersion, version)) {
+    console.log(
+      "The version in the checked out code is already higher or equal",
+      oldVersion
+    );
+    return false;
+  }
+
+  packageJson["op_version"] = version;
+
+  writeFileSync(
+    packageJsonPath,
+    JSON.stringify(packageJson, undefined, "  ") + os.EOL,
+    { encoding: "utf8" }
+  );
+
+  return true;
+}
+
+/**
+ *
+ * @param {string} command
+ * @param {Array<string>} args
+ * @param {import('child_process').SpawnSyncOptionsWithStringEncoding} options
+ */
+function spawn(command, args, options) {
+  console.log(`${command} ${args.join(" ")}`);
+
+  const child = spawnSync(command, args, {
+    encoding: "utf8",
+    cwd: cloneDir, // <-- most of the commands below work in this directory, except gitClone
+    env: process.env,
+    ...options
+  });
+
+  if (child.output) {
+    console.log(child.output.filter(item => typeof item === "string").join(""));
+  }
+
+  if (child.error) {
+    throw child.error;
+  }
+
+  console.log();
+
+  return child;
+}
+
+/**
+ *
+ * @param {string} command
+ * @param {Array<string>} args
+ * @param {import('child_process').SpawnSyncOptionsWithStringEncoding} options
+ */
+function spawnOutput(command, args, options) {
+  const result = spawn(command, args, options);
+
+  if (result.status !== 0) {
+    throw new Error(
+      `The exit status of the last command is not zero: ${result.status}`
+    );
+  }
+
+  return result.stdout;
+}
+
+/**
+ *
+ * @param {string} command
+ * @param {Array<string>} args
+ * @param {import('child_process').SpawnSyncOptionsWithStringEncoding} options
+ */
+function spawnStatus(command, args, options) {
+  return spawn(command, args, options).status;
+}
+
+function gitClone() {
+  if (existsSync(cloneDir)) {
+    console.log("Deleting", cloneDir);
+    rimraf.sync(cloneDir);
+  }
+  spawnOutput("git", ["clone", "-b", branch, url, cloneDirName], {
+    cwd: rootDir
+  });
+}
+
+function gitBranchExists(b) {
+  const status = spawnStatus("git", ["show-branch", `origin/${b}`]);
+  return status === 0;
+}
+
+function gitCheckout() {
+  const branchExists = gitBranchExists(pushBranch);
+  if (branchExists) {
+    spawnOutput("git", ["checkout", pushBranch]);
+  } else {
+    spawnOutput("git", ["checkout", "-b", pushBranch]);
+  }
+}
+
+function gitAdd() {
+  spawnOutput("git", ["add", "package.json"]);
+}
+
+function gitStatus() {
+  return spawnOutput("git", ["status"]);
+}
+
+function gitCommit() {
+  spawnOutput("git", ["commit", "-m", "fix: upgrade op cli version"]);
+}
+
+function gitPush() {
+  spawnOutput("git", ["push", "--dry-run", url, `HEAD:${pushBranch}`]);
+}
+
+determineLatestVersion().then(version => {
+  if (!gt(version, packageOpVersion)) {
+    console.log("No updates");
+    return;
+  }
+
+  gitClone();
+
+  gitCheckout();
+
+  if (!updatePackageJsonIfRequired(version)) {
+    return;
+  }
+
+  gitAdd();
+
+  if (/nothing to commit, working tree clean/.test(gitStatus())) {
+    return;
+  }
+
+  gitCommit();
+
+  gitPush();
+});
---
+++ op-update-watcher/determineLatestVersion.js
@@ -0,0 +1,72 @@
+const chrome = require("selenium-webdriver/chrome");
+const { Builder } = require("selenium-webdriver");
+const { Query } = require("webdriver-query");
+const { gt, eq, parse } = require("semver");
+require("./prepareChromedriver");
+
+const checkPage = "https://app-updates.agilebits.com/product_history/CLI";
+
+/**
+ * @template T
+ * @param {Promise<T>} promise
+ * @param {() => Promise<void>} cb
+ * @returns {Promise<T>}
+ */
+function withFinally(promise, cb) {
+  return promise
+    .catch(err => cb().then(() => Promise.reject(err)))
+    .then(value => cb().then(() => Promise.resolve(value)));
+}
+
+function determineLatestVersion() {
+  const options = new chrome.Options()
+    .detachDriver(false)
+    .excludeSwitches("enable-logging")
+    .addArguments("--silent")
+    .headless();
+
+  const headlessChrome = new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
+
+  const query = new Query(headlessChrome);
+
+  const versionRegex = /\d+\.\d+\.\d+/;
+
+  const versionNumber = query
+    .get(checkPage)
+    .then(() =>
+      query
+        .findElements("article > h3")
+        .map(header => header.getText())
+        .perform()
+    )
+    .then(headers => {
+      const versions = headers
+        .map(text => {
+          const result = versionRegex.exec(text);
+          return result && result[0];
+        })
+        .filter(item => !!item)
+        .map(item => parse(item));
+
+      versions.sort((a, b) => {
+        if (eq(a, b)) {
+          return 0;
+        } else if (gt(a, b)) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+
+      return Promise.resolve(versions[0].version);
+    });
+
+  return withFinally(versionNumber, () => headlessChrome.quit());
+}
+
+module.exports = {
+  determineLatestVersion
+};
